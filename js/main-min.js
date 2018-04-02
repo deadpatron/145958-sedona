19c87712408cbf23f2a2b90804390d8c93d04f9c
deadpatron <31518999+deadpatron@users.noreply.github.com>
2018-04-02 21:41:05 +0300

    вроде всё
    добавлен js
    исправлены мелкие ошибки

---
+++ js/main-min.js
@@ -0,0 +1,1 @@
+var formOn=document.querySelector(".form-container");var seachButton=document.querySelector(".rooms-search-button");formOn.classList.add("show-off");seachButton.addEventListener("click",function(evt){evt.preventDefault();formOn.classList.toggle("show-off")})
